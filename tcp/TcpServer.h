7cdd4c5468ad8d89f689cd12d656040602104c84
sunke <282641560@qq.com>
2024-09-01 19:41:53 +0800

    tcpserver + tcpconnnection

---
+++ tcp/TcpServer.h
@@ -0,0 +1,28 @@
+#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include "EventLoop.h"
+#include "ThreadPool.h"
+
+class TcpServer {
+public:
+    TcpServer(unsigned short port, int threadNum);
+
+    // 初始化监听
+    void setListen();
+
+    // 启动服务器
+    void run();
+
+    static int acceptConnection(void *arg);
+
+private:
+    int m_threadNum;
+    EventLoop *m_mainLoop;
+    ThreadPool *m_threadPool;
+    int m_lfd{};
+    unsigned short m_port;
+};
+
+
+#endif //TCPSERVER_H
